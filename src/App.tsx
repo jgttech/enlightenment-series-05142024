5c3e07f98fde82665fbade86cd55099fa2b38440
Jonathon Tech <jonathon.tech@icfnext.com>
2024-05-14 09:55:48 -0500

    Update sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Box } from "~/components/Box";
+
 export function App() {
-  return <div className="bg-red-500 pl-[101px]">Hello World</div>;
+  return <Box tw="bg-red-500 pl-[101px]">Hello World</Box>;
 }
